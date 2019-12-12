0405372e986999ab2e41fedc128c5abe0946fc48
Adam Younes <54439799+AdamYounes@users.noreply.github.com>
2019-12-12 11:27:46 +0100

    First three questions

---
+++ exercises.py
@@ -0,0 +1,31 @@
+#Question 1
+
+#import time
+
+#x = 0 
+#numbers = True
+
+
+#while numbers:
+#    print(x)
+#    time.sleep(1)
+#    x += 1
+
+#print(numbers)
+
+#Question 2
+
+#def questionify(x):
+#    return(x +"?")
+
+#print(questionify("Jag heter Adam"))
+
+#Question 3
+#def divisible_by_five():
+
+
+#Question 4
+def angry(x):
+    return x.upper() + "!!!"
+
+print(angry("hejsan"))
