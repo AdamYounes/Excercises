998fcee4d850ac91e44c0e990ecb7bbff2795884
Adam Younes <54439799+AdamYounes@users.noreply.github.com>
2019-12-15 19:17:31 +0100

    Exercises during break

--- exercises.py
+++ exercises.py
@@ -20,12 +20,34 @@
 
 #print(questionify("Jag heter Adam"))
 
-#Question 3
-#def divisible_by_five():
+#Question 3 New(under helgen)
+
+# def divisible_by_five(number):
+#     answer = number/5
+#     print(answer.is_integer())
+#     return number
+
+# divisible_by_five(25)
 
 
 #Question 4
-def angry(x):
-    return x.upper() + "!!!"
+#def angry(x):
+#    return x.upper() + "!!!"
 
-print(angry("hejsan"))
+#print(angry("hejsan"))
+
+#Question 5 New(under helgen)
+# the_one = ["Yo"]
+
+# def copy_list(the_one):
+#     return the_one
+
+# print(copy_list(the_one))
+
+#Question 7 New(under helgen)
+
+#my_list = []
+#for i in range(50):
+#    my_list.append('adam')
+#print(my_list)
+
